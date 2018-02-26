68d96de3d80a8a72ffeffa1c3613d61a95ca3c91
arielshr <36861197+arielshr@users.noreply.github.com>
2018-02-26 11:57:34 -0800

    Add files via upload

---
+++ my_folder/Class-1.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+genreg <- function(n){
+  x1 <- rnorm(n)
+  x2 <- rnorm(n)
+  eps <- rnorm(n)
+  y <- 5-x1+2*x2+eps
+  tibble(x1=x1, x2=x2, y=y)
+}
+
+sample <- genreg(100)
+sample
